c562b8faf52698af19cbc989b58b9e09eba2b6aa
eswar21-sudo <eswarkumar5475@gmail.com>
2025-03-20 13:06:24 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,5 +1,5 @@
-resource "spacelift_datadog" "example" {
-  
+module "spacelift_datadog" {
+  source = "spacelift-io/datadog/spacelift"
   dd_api_key = c71f343e2618aaa5f6702a12e8f3a233
   dd_site = "datadoghq.com"
   space_id = "root"
