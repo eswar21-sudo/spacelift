605e6c5dc0077065de296364b3669b1d9e6c2ab7
eswar21-sudo <eswarkumar5475@gmail.com>
2025-03-20 12:44:31 +0530

    Create main.tf

---
+++ main.tf
@@ -0,0 +1,9 @@
+module "spacelift_datadog" {
+  source = "spacelift-io/datadog/spacelift"
+
+  dd_api_key = c71f343e2618aaa5f6702a12e8f3a233
+  dd_site = "datadoghq.com"
+  space_id = "root"
+  extra_tags = {"env":"prod"}
+  exclude_tags = ["run_note", "run_url"]
+}
